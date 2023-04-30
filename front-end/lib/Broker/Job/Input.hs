328520358630cc7978b6f2dbda75ef47d2760f0b
Recursion Ninja <github@recursion.ninja>
2023-04-30 18:57:19 -0400

    Functional TUI for sending JSON request

--- front-end/lib/Broker/Job/Input.hs
+++ front-end/lib/Broker/Job/Input.hs
@@ -8,42 +8,220 @@
 {-# Language TemplateHaskell #-}
 
 module Broker.Job.Input
-  ( -- * Data-types
-    UserInputJob()
-  , UserInput(..)
+  ( -- * JobInput
+    JobInput()
     -- ** Constructor
-  , userInputEmptyJob
-    -- * Lenses
-  , HasJobCPUs(..)
-  , HasJobData(..)
-  , HasJobDisk(..)
-  , HasJobGPUs(..)
-  , HasJobMail(..)
-  , HasJobRAM(..)
-  , HasJobTask(..)
-  , HasJobTime(..)
+  , blankJobInput
+    -- ** Lenses
+  , jobInputEmail
+  , jobInputEdict
+  , jobInputLevel
+    -- * Other Data-types
+  , Disk()
+  , fromBytes
+  , Email()
+  , Minutes()
+  , Priority(..)
   ) where
 
 
-import Broker.Job.Class
-import Broker.Job.Input.Internal(UserInputJob(..), UserInput(..), emptyUserInput)
-import Broker.Job.Generic(JobBinaryStreamsT(..), JobMetadataT(..), JobSpecificationT(JobSpecificationT))
+import Control.Monad (replicateM, when)
+import Data.Aeson.Types
+import Data.Char (isSpace)
+import Data.Text (Text, pack, unpack)
+import Data.Word
+import GHC.Generics
+import Lens.Micro.TH
+import Text.Read (get, look, pfail, readPrec, readListPrec, readListPrecDefault)
 
 
-userInputEmptyJob :: UserInputJob
-userInputEmptyJob =
-    let emptyStreams = JobBinaryStreamsT
-            { jobStreamTask = emptyUserInput
-            , jobStreamData = emptyUserInput
-            }
+data  JobInput
+    = JobInput
+    { _jobInputEmail :: Email
+    , _jobInputEdict :: Minutes
+    , _jobInputLevel :: Priority
+    }
 
-        emptyMetadata = JobMetadataT
-            { jobMetaMail = ""
-            , jobMetaTime = emptyUserInput
-            , jobMetaDisk = emptyUserInput
-            , jobMetaRAM  = emptyUserInput
-            , jobMetaCPUs = emptyUserInput
-            , jobMetaGPUs = emptyUserInput
-            }
 
-    in  UserInputJob $ JobSpecificationT emptyMetadata emptyStreams
+blankJobInput :: JobInput
+blankJobInput = JobInput (Email "") 30 Low -- ""
+
+
+newtype Disk = Disk Word64
+
+
+newtype Email = Email Text
+
+
+newtype Minutes = Minutes Word64
+
+
+data  Priority
+    = Nil
+    | Low
+    | Mid
+    | Max
+
+
+deriving newtype instance Enum Disk
+
+
+deriving newtype instance Enum Minutes
+
+
+deriving newtype instance Eq Disk
+
+
+deriving newtype instance Eq Email
+
+
+deriving stock   instance Eq JobInput
+
+
+deriving newtype instance Eq Minutes
+
+
+deriving stock   instance Eq Priority
+
+
+deriving newtype instance FromJSON Disk
+
+
+deriving newtype instance FromJSON Email
+
+
+deriving newtype instance FromJSON Minutes
+
+
+deriving stock   instance Generic Priority
+
+
+deriving newtype instance Integral Disk
+
+
+deriving newtype instance Integral Minutes
+
+
+deriving newtype instance Num Disk
+
+
+deriving newtype instance Num Minutes
+
+
+deriving newtype instance Ord Disk
+
+
+deriving newtype instance Ord Email
+
+
+deriving stock   instance Ord JobInput
+
+
+deriving newtype instance Ord Minutes
+
+
+deriving stock   instance Ord Priority
+
+
+deriving stock   instance Read JobInput
+
+
+deriving newtype instance Read Disk
+
+
+deriving newtype instance Read Minutes
+
+
+deriving stock   instance Read Priority
+
+
+deriving newtype instance Real Disk
+
+
+deriving newtype instance Real Minutes
+
+
+deriving stock   instance Show JobInput
+
+
+deriving newtype instance Show Minutes
+
+
+deriving stock   instance Show Priority
+
+
+deriving newtype instance ToJSON Disk
+
+
+deriving newtype instance ToJSON Email
+
+
+deriving newtype instance ToJSON Minutes
+
+
+instance FromJSON Priority where
+
+    parseJSON v@(Number n) =
+        case n of
+            0 -> pure Nil
+            1 -> pure Low
+            2 -> pure Mid
+            3 -> pure Max
+            _ -> prependFailure "parsing Priority failed, " $ unexpected v
+
+    parseJSON invalid = prependFailure "parsing Priority failed, " $
+       typeMismatch "Number" invalid
+
+
+
+instance ToJSON Priority where
+
+    toJSON Nil = Number 0
+    toJSON Low = Number 1
+    toJSON Mid = Number 2
+    toJSON Max = Number 3
+
+    toEncoding = genericToEncoding defaultOptions
+
+
+instance Read Email where
+
+    {-# INLINABLE readPrec #-}
+    readPrec = do
+        input <- look
+        let token = takeWhile (not . isSpace) input
+        _ <- replicateM (length token) get
+        let (a,b) = break (== '@') token
+        let (c,d) = break (== '.') b
+        let e     = drop 1 d
+        when (null e) pfail
+        pure . Email . pack $ token
+
+    readListPrec = readListPrecDefault
+
+
+instance Show Disk where
+
+    {-# INLINABLE show #-}
+    show (Disk kibiB) =
+        let (num,unit) = case kibiB `quotRem` 1024 of
+                (0,r)     -> (r  , "KiB")
+                (mebiB,r) ->
+                    case (mebiB + 1) `quotRem` 1024 of
+                      (0,r) -> (r, "MiB") 
+                      (g,_) -> (g, "GiB")
+            
+        in  unwords [ show num, unit ]
+
+
+instance Show Email where
+
+    {-# INLINABLE show #-}
+    show (Email txt) = unpack txt
+
+
+fromBytes :: Integral i => i -> Disk
+fromBytes bytes = Disk . fromIntegral $ bytes `div` 1024
+
+
+makeLenses ''JobInput
--- front-end/lib/Broker/Job/Specification.hs
+++ front-end/lib/Broker/Job/Specification.hs
@@ -11,84 +11,44 @@ module Broker.Job.Specification
   ( -- * JobSpecification
     JobSpecification()
     -- ** Constructor
-  , fromValidUserInput
-    -- * JobError
-  , JobError(..)
+  , buildJobSpecification
     -- * Lenses
-  , HasJobCPUs(..)
-  , HasJobData(..)
-  , HasJobDisk(..)
-  , HasJobGPUs(..)
-  , HasJobMail(..)
-  , HasJobRAM(..)
-  , HasJobTask(..)
-  , HasJobTime(..)
+  , jobSpecEmail
+  , jobSpecTime
+  , jobSpecPriority
+  , jobSpecFiles
   ) where
 
 
-import Broker.Job.Class
-import Broker.Job.Input.Internal
-import Broker.Job.Generic
-import Broker.Job.Specification.Internal
-import Data.Functor.Identity
-import Data.List.NonEmpty(NonEmpty(..))
-import Data.Text qualified as T
-import Data.Validation
-import Lens.Micro (Getting, (^.))
+import Broker.Job.Input
+import Data.Aeson.Types
+import Data.Aeson.TH
+import Data.Char (toLower)
+import Data.Foldable
+import Lens.Micro ((^.))
+import Lens.Micro.TH
 
 
-newtype JobError = JobError { getJobErrors :: NonEmpty String }
+data  JobSpecification
+    = JobSpecification
+    { _jobSpecEmail    :: Email
+    , _jobSpecTime     :: Minutes
+    , _jobSpecPriority :: Priority
+    , _jobSpecFiles    :: [(FilePath, Disk)]
+    }
 
 
-deriving newtype instance Eq JobError
+buildJobSpecification :: Foldable f => f (FilePath, Disk) -> JobInput -> JobSpecification
+buildJobSpecification files input =
+    JobSpecification
+    { _jobSpecEmail    = input ^. jobInputEmail
+    , _jobSpecTime     = input ^. jobInputEdict
+    , _jobSpecPriority = input ^. jobInputLevel
+    , _jobSpecFiles    = toList files
+    }
 
 
-deriving newtype instance Ord JobError
+makeLenses ''JobSpecification
 
 
-deriving newtype instance Semigroup JobError
-
-
-deriving newtype instance Show JobError
-
-
-fromValidUserInput :: UserInputJob -> Validation JobError JobSpecification
-fromValidUserInput (UserInputJob spec) =
-    let meta :: JobMetadataT UserInput
-        meta = getJobMetadata spec
-
-        bins :: JobBinaryStreamsT UserInput
-        bins = getJobStreams spec
-
-        check :: (String, Getting (UserInput a) s (UserInput a)) -> s -> Validation JobError (Identity a)
-        check (key, getter) obj = case getUserInput $ obj ^. getter of
-            Nothing -> Failure . JobError $ pure key
-            Just v  -> Success $ Identity v
-
-        email :: (String, Getting T.Text p T.Text) -> p -> Validation JobError T.Text
-        email (key, getter) obj =
-            let txt = obj ^. getter
-            in  if T.null txt
-                then Failure . JobError $ pure key
-                else Success txt                                                                                                   
-
-
-
-        checkMeta :: JobMetadataT UserInput -> Validation JobError (JobMetadataT Identity)
-        checkMeta obj = JobMetadataT
-            <$> email ( "Mail", jobMail ) obj
-            <*> check ( "Time", jobTime ) obj
-            <*> check ( "Disk", jobDisk ) obj
-            <*> check ( "RAM" , jobRAM  ) obj
-            <*> check ( "CPUs", jobCPUs ) obj
-            <*> check ( "GPUs", jobGPUs ) obj
-
-        checkBins :: JobBinaryStreamsT UserInput -> Validation JobError (JobBinaryStreamsT Identity)
-        checkBins obj = JobBinaryStreamsT
-            <$> check ( "Task", jobTask ) obj
-            <*> check ( "Data", jobData ) obj
-
-
-    in  fmap JobSpecification $ JobSpecificationT
-            <$> checkMeta meta
-            <*> checkBins bins
+$(deriveJSON defaultOptions{fieldLabelModifier = drop 8 . map toLower, constructorTagModifier = map toLower} ''JobSpecification)
